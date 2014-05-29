eb35e10cab9d2f222523227aab67d7342a7c7b57
joejochen <joe.jochen89@gmail.com>
2014-05-29 21:25:10 +0200

    1.commoit

--- testClient/src/testClient/ConnectionTest02.java
+++ testClient/src/testClient/ConnectionTest02.java
@@ -1,18 +1,26 @@
 package testClient;
 
 import java.io.IOException;
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ResponseHandler;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.BasicResponseHandler;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.message.BasicNameValuePair;
+
 
 public class ConnectionTest02 {
 	
@@ -22,15 +30,45 @@ public class ConnectionTest02 {
 	
 	public static  void main(String[] args) {
 		// TODO Auto-generated method stub
-		CloseableHttpClient hc = HttpClientBuilder.create().build();
-		ResponseHandler <String> res = new BasicResponseHandler();  
+		//CloseableHttpClient hc = HttpClientBuilder.create().build();
+		//ResponseHandler <String> res = new BasicResponseHandler();  
+		final HttpClient httpClient = new HttpClient();
 		
 		String postMessage = "jr in Json Test";
+		User user = new User("hans12", "342");
 		
-		HttpPost postMethod=new HttpPost("http://localhost:8080/logs");  
+		String jsonString = user.toJson();
+		
+		try {
+			
+			
+			StringRequestEntity requestEntity = new StringRequestEntity(jsonString,
+				    "application/json",
+				    "UTF-8");
+			
+			
+			System.out.println(requestEntity.toString());
+			PostMethod postMethod = new PostMethod("http://localhost:8080/cp418/usermanagement");
+			postMethod.setRequestEntity(requestEntity);
+			int status = httpClient.executeMethod(postMethod);
+			
+			System.out.println("status: " + status);
+			//getResponseBodyAsStream
+			String tmp = postMethod.getResponseBodyAsString();
+			System.out.println("responseBody: " + tmp);
+			postMethod.releaseConnection();
+			
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		/*
+		HttpPost postMethod=new HttpPost("http://localhost:8080/cp418/usermanagement/createPost");  
 		List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1); 
 		
-		nameValuePairs.add(new BasicNameValuePair("json", postMessage));
+		
+		nameValuePairs.add(new BasicNameValuePair("json","test"));
+		
 		
 		try {
 			postMethod.setEntity(new UrlEncodedFormEntity(nameValuePairs));
@@ -46,7 +84,7 @@ public class ConnectionTest02 {
 		      while ((line = rd.readLine()) != null) {
 		        System.out.println(line);
 		      }*/
-			
+		/*	
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -57,6 +95,13 @@ public class ConnectionTest02 {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
+		}
+		*/ catch (HttpException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 	}
 
--- testClient/src/testClient/ConnectionTest03.java
+++ testClient/src/testClient/ConnectionTest03.java
@@ -1,5 +1,45 @@
 package testClient;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class ConnectionTest03 {
 
+	
+	public static  void main(String[] args) {
+		User user = new User();
+		HttpURLConnection urlCon;
+		ObjectOutputStream out;
+		URL url;
+		try {
+			url = new URL("http://localhost:8080/cp418/usermanagement");
+			urlCon = (HttpURLConnection) url.openConnection();
+			urlCon.setRequestMethod("POST");
+		    urlCon.setDoOutput(true); // to be able to write.
+		    urlCon.setDoInput(true); // to be able to read.
+		    
+
+		    out = new ObjectOutputStream(urlCon.getOutputStream());
+		    out.writeObject(user);
+		    out.close();
+		    
+		    ObjectInputStream ois = new ObjectInputStream(urlCon.getInputStream());
+		    user = (User) ois.readObject();
+		    ois.close();
+			
+		} catch (MalformedURLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
