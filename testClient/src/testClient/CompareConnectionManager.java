cdb28e4694cc979241a4c963576d3e71845fd918
joejochen <joe.jochen89@gmail.com>
2014-06-01 23:48:24 +0200

    updatet TestClient

--- testClient/src/testClient/CompareConnectionManager.java
+++ testClient/src/testClient/CompareConnectionManager.java
@@ -2,12 +2,15 @@ package testClient;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 
 import org.apache.commons.httpclient.HostConfiguration;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.StringRequestEntity;
+import org.apache.commons.httpclient.params.HttpMethodParams;
 
 public class CompareConnectionManager {
 	
@@ -20,7 +23,15 @@ public class CompareConnectionManager {
 		
 		return jsonString;
 	}
-	
+	public static String createRoute(String userName,int duration, int length, String type, double CO2, double costs, Date date,
+            int referencelength, double referenceco2, double referencecosts){
+		LoggedRoute user = new LoggedRoute(userName, duration, length,  type, CO2,  costs, date,
+	            referencelength,  referenceco2, referencecosts);
+		
+		String jsonString = user.toJson();
+		
+		return jsonString;
+	}
 	
 	public static int connection( final String jsonString, final String url){
 		
@@ -58,8 +69,72 @@ public class CompareConnectionManager {
 			e.printStackTrace();
 		}
 		
+		return status;
+		
+	}
+	public static int connectionGET(final String url){
+		GetMethod getMethod = null;
+		HttpClient httpClient = new HttpClient();
 		
 		
+		int status = 0;
+		try {
+			
+			//87.106.189.151
+			//http://87.106.189.151:8080/cp422/usermanagement
+			getMethod = new GetMethod(url);
+		
+			
+			status = httpClient.executeMethod(getMethod);
+			
+			System.out.println("status: " + status);
+			
+			String tmp = getMethod.getResponseBodyAsString();
+			System.out.println("responseBody: " + tmp);
+			
+			getMethod.releaseConnection();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}	
+		
+		return status;
+	}
+public static int connectionWithString( final String str, final String url){
+		
+		PostMethod postMethod = null;
+		HttpClient httpClient = new HttpClient();
+		//HostConfiguration hf=new HostConfiguration();
+		//hf.setHost("http://localhost", 8080);
+		
+		
+		int status = 0;
+		try {
+			
+			
+			//87.106.189.151
+			//http://87.106.189.151:8080/cp422/usermanagement
+			postMethod = new PostMethod(url);
+			//postMethod.setHostConfiguration(hf);
+			StringRequestEntity requestEntity = new StringRequestEntity(str);
+			System.out.println(requestEntity.toString());
+			postMethod.setRequestEntity(requestEntity);
+			
+			
+			status = httpClient.executeMethod(postMethod);
+			
+			System.out.println("status: " + status);
+			
+			String tmp = postMethod.getResponseBodyAsString();
+			System.out.println("responseBody: " + tmp);
+			
+			postMethod.releaseConnection();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
 		return status;
 		
--- testClient/src/testClient/ConnectionTestLocal.java
+++ testClient/src/testClient/ConnectionTestLocal.java
@@ -1,84 +1,52 @@
 package testClient;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-import java.net.URL;
-import java.net.URLConnection;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-public class ConnectionTest {
-	 
+
+
+
+public class ConnectionTestLocal {
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		String host = "http://87.106.189.151:8080/cp423/usermanagement/";
+		String host = "http://localhost:8080/cp423/usermanagement/";
 		
 		//creates User on Client
-
 		//right user
 		String userClient = CompareConnectionManager.createUser("ranger1966", "23453sf");
 		//user not saved in Database = wrong username
 		String userClient2 = CompareConnectionManager.createUser("rangerWrong", "23453sf");
 		// user with wrong password
 		String userClient3 = CompareConnectionManager.createUser("ranger1966", "WrongPassword");
-				
+		
 		//Sends User as Json to spring Controller and then save User to Database
 		CompareConnectionManager.connection(userClient, host + "Register");
-				
+		
 		//should return 406, because User already registered
 		CompareConnectionManager.connection(userClient, host + "Register");
-				
-				
+		
+		
 		//Sends User as Json to spring Controller and checks, if User is in Database
 		CompareConnectionManager.connection(userClient, host + "Anmelden");
 
 		//user not saved in Database = wrong username
 		CompareConnectionManager.connection(userClient2, host + "Anmelden");
-				
+		
 		// user with wrong password
 		CompareConnectionManager.connection(userClient3, host + "Anmelden");
-				
+		
 		//Changes Passwort of given User
 		//Checks, if old passwort is korrekt
 		//CompareConnectionManager.connection(userClient, host + "changePassword");
-						
-				
-		// Delete given User
-		//CompareConnectionManager.connection(userClient, host + "Delete");
-				
-		//should return 404 user not found
-		//CompareConnectionManager.connection(userClient, host + "Delete");
+			
 		
-		/*	Gewünschtes Ergebnis:
-		 	org.apache.commons.httpclient.methods.StringRequestEntity@6659c656
-			status: 200
-			responseBody: {"user_name_id":"ranger1966","user_pw":"23453sf"}
-			org.apache.commons.httpclient.methods.StringRequestEntity@2530c12
-			status: 406
-			responseBody:  User is already Registered! 
-			org.apache.commons.httpclient.methods.StringRequestEntity@73c6c3b2
-			status: 200
-			responseBody: {"user_name_id":"ranger1966","user_pw":"23453sf"}
-			org.apache.commons.httpclient.methods.StringRequestEntity@48533e64
-			status: 404
-			responseBody:  User Not Found! 
-			org.apache.commons.httpclient.methods.StringRequestEntity@64a294a6
-			status: 404
-			responseBody:  Password or User wrong!! 
-			org.apache.commons.httpclient.methods.StringRequestEntity@7e0b37bc
-			status: 200
-			responseBody: {"user_name_id":"ranger1966","user_pw":"23453sf"}
-			org.apache.commons.httpclient.methods.StringRequestEntity@3b95a09c
-			status: 404
-			responseBody: User not Found
-		*/
+		
 		System.out.println("!----------Route Test--------------!");
 		
-		String hostroute = "http://87.106.189.151:8080/cp423/routemanagement/";
+		String hostroute = "http://localhost:8080/cp423/routemanagement/";
 		
 		Date date = new Date(23424);
 		System.out.println(date);
@@ -102,7 +70,7 @@ public class ConnectionTest {
 		CompareConnectionManager.connection(testRoute, hostroute + "showRoutePerUser");
 		
 		//Suche nach Route mit Id
-		//CompareConnectionManager.connectionGET("http://87.106.189.151:8080/cp423/routemanagement/findRouteWithId?id=2");
+		CompareConnectionManager.connectionGET("http://localhost:8080/cp423/routemanagement/findRouteWithId?id=2");
 		
 		//gibt alle Routen zurück, die angegebenen Usernamen zugeordnet sind
 		CompareConnectionManager.connectionWithString(username, hostroute + "username");
